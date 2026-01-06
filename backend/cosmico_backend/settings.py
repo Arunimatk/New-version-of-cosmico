244a8bbd1e7be961c59779997e98a87effcd93a0
arunima <arunimatka@gmail.com>
2026-01-06 11:14:10 +0530

    Cleanup settings.py duplicates

--- backend/cosmico_backend/settings.py
+++ backend/cosmico_backend/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
