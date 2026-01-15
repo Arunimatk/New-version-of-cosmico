f243978ed4d3b879676a6e3ea7acec756cc5da65
arunima <arunimatka@gmail.com>
2026-01-15 21:10:34 +0530

    Fix API baseURL to include /api prefix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cosmico-backend-arun.onrender.com',
+  baseURL: 'https://cosmico-backend-arun.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
